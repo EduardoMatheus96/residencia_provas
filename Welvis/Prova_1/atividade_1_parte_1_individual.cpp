ca425946333b4306051e8f67f4465e7b540bf6c8
WelisSS <welvisconta@gmail.com>
2023-09-18 09:36:33 -0300

    criacao da funcao de insercao

--- Welvis/Prova_1/atividade_1_parte_1_individual.cpp
+++ Welvis/Prova_1/atividade_1_parte_1_individual.cpp
@@ -19,19 +19,44 @@ struct Roteiro{
     string destino;
 };
 
+Passageiro inserirPassageiro(){
+
+    Passageiro passageiro;
+
+    cout << "Insira se CPF: " << endl;
+    cin >> passageiro.CPF;
+    cout << "Insira seu Nome: " << endl;
+    cin >> passageiro.nome;
+    cout << "Insira sua data de Nascimento 00/00/0000: " << endl;
+    cin >> passageiro.dtNascimento;
+    cout << "Insira seu numero de autorizacao: " << endl;
+    cin >> passageiro.numAutorizacao;
+
+    return passageiro;
+
+}
+
 
 
 int main(void){
 
-    string menuOptions = "(1) Incluir\n(2) Excluir\n(3) Alterar(Apenas por CPF)\n(4) Listar\n(5) Localizar (por CPF)\n(0) Sair";
-    cout << menuOptions << endl;
+    string opcoesDoMenu = "(1) Incluir\n(2) Excluir\n(3) Alterar(Apenas por CPF)\n(4) Listar\n(5) Localizar (por CPF)\n(0) Sair";
+    cout << opcoesDoMenu << endl;
     int selectedOption;
+
+    vector<Passageiro> passageiros;
+    vector<Roteiro> roteiro;
 
     do {
         cin >> selectedOption;
 
         if(selectedOption != 0){
-            cout << menuOptions << endl;
+            cout << opcoesDoMenu << endl;
+
+            if(selectedOption == 1)
+                passageiros.push_back(inserirPassageiro());
+            
+            
         }
 
     }while(selectedOption != 0);
