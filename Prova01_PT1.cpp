c780374978c8a2242fed9701ae3f857d0b7e6d98
giuseppemota <75266680+giuseppemota@users.noreply.github.com>
2023-09-20 09:13:49 -0300

    Adicionando a Função MaiorIdade e IncluirPassageiro
    Adicionando a função que verifica a idade do passageiro e a função que adiciona o passageiro ao vector.

--- Prova01_PT1.cpp
+++ Prova01_PT1.cpp
@@ -23,3 +23,45 @@ struct roteiro
     string Destino;
 };
 
+bool MaiorIdade(const string DtNascimento) {
+    // Obter a data atual
+    time_t now;
+    time(&now);
+    tm t = *gmtime(&now);
+
+    // Extrair o ano, mês e dia da data de nascimento
+    int anoNascimento = stoi(DtNascimento.substr(6, 4));
+    int mesNascimento = stoi(DtNascimento.substr(3, 2));
+    int diaNascimento = stoi(DtNascimento.substr(0, 2));
+
+    // Calcular a idade
+    int anoAtual = t.tm_year + 1900;
+    int mesAtual = t.tm_mon + 1;
+    int diaAtual = t.tm_mday;
+
+    int idade = anoAtual - anoNascimento;
+
+    // Verificar se o aniversário já ocorreu este ano
+    if (mesAtual < mesNascimento || (mesAtual == mesNascimento && diaAtual < diaNascimento)) {
+        idade--;
+    }
+
+    // Verificar se a idade é menor ou igual a 18 anos
+    return idade <= 18;
+}
+
+void IncluirPassageiro(vector<passageiro> &dadosPassageiros){
+    passageiro temp;
+    cout << "Digite o Nome do Passageiro: " <<endl;
+    cin >> temp.Nome;
+    cout << "Digite o CPF do Passageiro sem caracteres: " << endl;
+    cin >> temp.CPF;
+    cout << "Digite a Data de Nascimento (formato: DD/MM/AAAA): " << endl;
+    cin >> temp.DtNascimento;    
+    if(MaiorIdade(temp.DtNascimento)){
+        cout << "Digite o numero de autorizacao: "<< endl;
+        cin >> temp.NumAutorizacao;
+    }
+    dadosPassageiros.push_back(temp);
+}
+
